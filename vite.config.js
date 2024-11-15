d1d9f91ef100c70e47fc02bf52fefd2921bbd5e5
Rajesh <rajeshky123456@gmail.com>
2024-11-15 14:38:23 +0545

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+<<<<<<< HEAD
    base: 'https://rajesh23965.github.io/NepWoodFrontend/',
 
+=======
+  base: '/NepWoodFrontend/',
+>>>>>>> bc8deac (Initial commit)
 })
