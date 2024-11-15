7a4bafee7a2e9d8e1abef30dc608bc37fef0f4cf
Rajesh Kumar Yadav <115943147+Rajesh23965@users.noreply.github.com>
2024-11-15 14:01:31 +0545

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  https://rajesh23965.github.io/NepWoodFrontend/
+   base: 'https://rajesh23965.github.io/NepWoodFrontend/',
+
 })
